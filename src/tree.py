a43aec3db4d106911ba180902c0657c37f8b0968
sofiakwok <zazakwok@gmail.com>
2022-03-01 22:19:05 -0800

    removing print statements and adding comments for depth search

--- src/tree.py
+++ src/tree.py
@@ -19,6 +19,8 @@ class Tree:
         # Loop.
 
         # check if we actually need to grow the tree (start may already be visible)
+        # searching from the most recently added node back along its parents
+        # probably want to change this a little bit to search from all nodes at the ends of branches
         startStateSearching = True
         n = self.tree[len(self.tree)-1]
         while startStateSearching:
@@ -30,6 +32,7 @@ class Tree:
                 startStateSearching = False
             if type(n.parent) != Node:
                 startStateSearching = False
+                break
             else: 
                 n = n.parent
 
@@ -72,7 +75,7 @@ class Tree:
                 return None
 
     def recursiveRemove(self, n):
-        print("remove:", n, "children:", n.children)
+        #print("remove:", n, "children:", n.children)
         while len(n.children) > 0:
             self.recursiveRemove(n.children[0])
         if n.parent is not None and n in n.parent.children:
